edb9bf442310d9b3ed88be8ed588ed21a20dc722
Skippity Paps <skippitypaps@users.noreply.github.com>
2023-06-13 23:27:46 -0700

    Playing with different ways to build up our crate stacks from the text input.

--- aoc-2022/src/aoc_2022/day5.clj
+++ aoc-2022/src/aoc_2022/day5.clj
@@ -55,16 +55,71 @@
      (partition 2 4)
      (map last))
 
+(defn parse-crate-line [line]
+  "Parameter 'line' represents a row in the stack of crates."
+  (some->> line
+           (partition 2 4)
+           (map second)))
+
+(->> crates
+     first
+     parse-crate-line)
+
+(map parse-crate-line crates) ;; it really is that easy
+
 ;; partitining by 4 we can just consume the spaces. The second character of every 4
 ;; characters would be the crate contents.
-;; [ a ] \space [ b ] \space [ \space  ]. empty crates replace brackets with spaces
+;; \space [ a ] \space [ b ] \space [ \space  ]. empty crates replace brackets with spaces
 ;; so
-;; [ a ] \space [ b ] \space \space \space \space
+;; \space [ a ] \space [ b ] \space \space \space
 ;; stride by 4
-;; [ a ] \space
-;; [ b ] \space
-;; [ \space ]
+;; \space [ a ]
+;; \space [ b ]
+;; \space [ \space ]
 ;; every second: a b \space
+
+;; the above ramblings about partition strides and crate contents aren't very 
+;; great comments... I kind of get what I was trying to convey.
 (map second (partition 4 (first crates)))
 (map last (partition 2 4 (first crates)))
 (concat [\space] [] [\V] [\V] [\Q] [\M] [\L] [\N] [\R])
+
+;; so where are we right now? 
+;; we need to take the list of moves and parse them into something meaningful.
+;; right now we're just reading crates? We're trying to build up a data struct
+;; to apply the 'moves' to. 
+
+(reverse crates)
+;; walk in reverse so we can build up our crate stacks easily. 
+;; assoc-in crate column/stack index per newline and we will have
+;; our stacks ready to be moved around. 
+
+crate-indices
+
+(rest
+ (map parse-crate-line (reverse crates)))
+
+(def crate-stacks
+  (zipmap crate-indices (repeat (count crate-indices) [])))
+
+;;(apply merge-with conj [crate-stacks test-map test-map2])
+
+;; all of this repeated use of '(zipmap crate-indices ...) has me thinking
+;; we can create a helper partial such that: 
+(def to-crate-map (partial zipmap crate-indices))
+
+(apply merge-with conj crate-stacks
+       (->> crates
+            reverse
+            rest
+            (map parse-crate-line)
+            (map #(zipmap crate-indices %))))
+ ;; clean the above up to make it a little more readable, even if more verbose
+(defn create-crate-stacks [stacks]
+  (let [reverse-order-crates (rest (reverse crates))
+        crate-contents (map parse-crate-line reverse-order-crates)
+        crates-as-maps (map to-crate-map crate-contents)]))
+
+
+
+(map to-crate-map (map parse-crate-line (rest (reverse crates))))
